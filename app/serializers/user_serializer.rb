6436da282706fb7044cf29c185c9658092adb162
Andrew Pellum <apellum19@gmail.com>
2021-12-16 10:38:58 -0600

    adjusted serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :name, :username, :password_digest, :date_of_birth
+  attributes :id, :email, :name, :username, :date_of_birth
   
 end
